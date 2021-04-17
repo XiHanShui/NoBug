630adfb3ac0262eba5b79eb40bef710c97d0f51c
WJ <wj@socct.work>
2021-04-17 16:17:10 +0800

    add ffmpegplayer

--- APT/app/src/main/java/com/socct/apt/MainActivity.kt
+++ APT/app/src/main/java/com/socct/apt/MainActivity.kt
@@ -2,7 +2,9 @@ package com.socct.apt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.socct.animation.ARouter
 
+@ARouter(value = "aaaaa")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ APT/compiler/src/main/java/com/socct/compiler/ARouterProcessor.java
@@ -0,0 +1,51 @@
+package com.socct.compiler;
+
+import com.google.auto.service.AutoService;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedOptions;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
+
+
+@AutoService(Processor.class)
+@SupportedAnnotationTypes("com.socct.animation.ARouter")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedOptions("content")
+public class ARouterProcessor extends AbstractProcessor {
+
+
+    private Elements elementUtils;
+    private Types typeUtils;
+    private Messager messager;
+    private Filer filer;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        elementUtils = processingEnv.getElementUtils();
+        typeUtils = processingEnv.getTypeUtils();
+        messager = processingEnv.getMessager();
+        filer = processingEnv.getFiler();
+        messager.printMessage(Diagnostic.Kind.NOTE, "test");
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+
+        return true;
+    }
+}
