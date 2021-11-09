0cf6898c0f2b5398e7a558612b333de50209c714
‘WJ’ <wj@socct.work>
2021-11-09 14:53:09 +0800

    new ffmpeg

--- FFmpegDemo/app/src/main/cpp/native-lib.cpp
+++ FFmpegDemo/app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_ffmpeg_player_MainActivity_stringFromJNI(
+Java_com_ffmpeg_ffmpegdemo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
